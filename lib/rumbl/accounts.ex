ebcb8dc4ad3b1ed48257f1efb9bd20b6e60eb6c2
fabianskier <fabianskier@icloud.com>
2022-11-22 00:45:32 -0300

    CO-0001: Add account and user context
    Signed-off-by: fabianskier <fabianskier@icloud.com>

---
+++ lib/rumbl/accounts.ex
@@ -0,0 +1,25 @@
+defmodule Rumbl.Accounts do
+  @moduledoc """
+  The Accounts context.
+  """
+
+  alias Rumbl.Accounts.User
+
+  def list_users do
+    [
+      %User{id: "1", name: "Oscar", username: "oscar.c"},
+      %User{id: "2", name: "Diana", username: "diana.f"},
+      %User{id: "3", name: "Benjamin", username: "benjamin.c"}
+    ]
+  end
+
+  def get_user(id) do
+    Enum.find(list_users(), fn map -> map.id == id end)
+  end
+
+  def get_user_by(params) do
+    Enum.find(list_users(), fn map ->
+      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
+    end)
+  end
+end
